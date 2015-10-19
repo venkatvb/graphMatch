52d717653dd694f71cf801825fd19e3448272bef
venkatvb <venkateshbabu95@gmail.com>
2015-10-19 22:59:12 +0530

    basic graph structure done

--- Matcher/src/graph/Graph.java
+++ Matcher/src/graph/Graph.java
@@ -1,5 +1,16 @@
 package graph;
 
-public class Graph {
+import java.util.ArrayList;
 
+public class Graph {
+	
+	ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+	
+	public boolean addNode(int nodeId) {
+		return true;
+	}
+	
+	public boolean addEdge(int nodeIdA, int nodeIdB) {
+		return true;
+	}
 }
---
+++ Matcher/src/graph/Node.java
@@ -0,0 +1,35 @@
+package graph;
+
+import java.util.ArrayList;
+
+public class Node {
+	public static final Double DEFAULT_DISTANCE = 0.0;
+	public int nodeId;
+	ArrayList<Edge> edges = new ArrayList<Edge>();
+	
+	public Node(int id) {
+		this.nodeId = id;
+	}
+	
+	void setNodeId(int id) {
+		this.nodeId = id;
+	}
+	
+	boolean addEdge(Integer destinationId, Double distance) {
+		// returns false if the added edge is a duplicate else returns true
+		// TODO adding each edge costs linear. Optimize it.
+		for(Edge edge : edges ) {
+			if(edge.getDestinationNodeId() == destinationId) {
+				// this destination is already added.
+				return false;
+			}
+		}
+		edges.add(new Edge(this.nodeId, destinationId, distance));
+		return true;
+	}
+	
+	boolean addEdge (Integer destinationId) {
+		// Adding the edge with the default distance.
+		return addEdge (destinationId, DEFAULT_DISTANCE);
+	}
+}	
