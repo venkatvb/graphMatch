b00a5c030032b8e8927144c9d042c0ef31abc4e1
venkatvb <venkateshbabu95@gmail.com>
2015-10-22 16:44:09 +0530

    Fixed logging

--- Matcher/src/logger/Log.java
+++ Matcher/src/logger/Log.java
@@ -1,5 +1,7 @@
 package logger;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
@@ -8,7 +10,6 @@ import java.util.Date;
 public class Log {
 	public static final String PATH_TO_LOG_FILE = "F:\\Graph Databases\\log\\log.txt";
 	public static final String ENCODING_FORMAT = "UTF-8";
-	public static boolean isConnected = false;
 	static PrintWriter writer;
 	
 	public static String getCurrentTimeStamp() {
@@ -19,37 +20,34 @@ public class Log {
 	}
 	
 	public static void debug(String message) throws IOException {
-		if(isConnected == false) {
-			establishConnection();
-		}
+		establishConnection();
 		writer.println("DEBUG : " + getCurrentTimeStamp() + ": " + message);
+		closeConnection();
 	}
 	
 	public static void error(String message) throws IOException {
-		if(isConnected == false) {
-			establishConnection();
-		}
+		establishConnection();
 		writer.println("ERROR : " + getCurrentTimeStamp() + ": " + message);
+		closeConnection();
 	}
 	
 	public static void info(String message) throws IOException {
-		if(isConnected == false) {
-			establishConnection();
-		}
+		establishConnection();
 		writer.println("INFO : " + getCurrentTimeStamp() + ": " + message);
+		closeConnection();
 	}
 	
 	public static boolean establishConnection() throws IOException {
 		// Returns true is the connection establishment is successful.
-		writer = new PrintWriter(PATH_TO_LOG_FILE, ENCODING_FORMAT);
-		isConnected = true;
+		writer = new PrintWriter(
+					new FileOutputStream(
+						new File(PATH_TO_LOG_FILE), 
+						true)
+					);
 		return true;
 	}
 	
 	public static void closeConnection() throws IOException {
-		if(isConnected == false) {
-			return;
-		}
 		writer.close();
 	}
 }
